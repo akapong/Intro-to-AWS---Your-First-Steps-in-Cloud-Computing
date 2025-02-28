977c52cbfda93a9d7924bd0652e820bae9d2cfff
akapong <41426546+akapong@users.noreply.github.com>
2025-02-28 15:35:48 +0700

    Update ec2-stress-util.sh

--- course-code-resources/course-code-resources/1-ec2-basics/ec2-stress-util.sh
+++ course-code-resources/course-code-resources/1-ec2-basics/ec2-stress-util.sh
@@ -1,3 +1,5 @@
+## Chapter 5
+## Section: [Hands-on] Creating a Scaling Policy and Stress Testing
 # Enable the EPEL (Extra Package for Enterprise Linux) repo
 # ==== This is for Linux 2 ====
 sudo amazon-linux-extras install epel -y # -y to accept all conditions
@@ -7,8 +9,6 @@ sudo yum install stress -y
 
 # Run the stress utility (with x number of CPU workers)
 sudo stress --cpu 5 # or 10, 12...
-
-
 
 # ===== For Linux 2023 (which is what we use in this course) Do the following =====:
 sudo yum update
